ac45c1a934866b7097c31be063dc8e1f5b4e2fea
hypnosw <hypnoswu@gmail.com>
2023-12-01 02:29:03 -0800

    set up app.js

--- app.js
+++ app.js
@@ -2,12 +2,29 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 
-console.log("Hello World!");
 
 const app = express();
 
-app.use(cors());
-
+app.use(
+    cors({   credentials: true,
+             origin: process.env.FRONTEND_URL
+         }
+    )
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
+console.log("Hello World!");
 
 app.listen(5000);
