2df1751fe072f3917dc9b8b61aaa624e050e4f9d
ccxxyy23 <ccxxyy23@outlook.com>
2023-12-02 01:10:37 -0800

    connect to remote db and create basic doc for users

--- app.js
+++ app.js
@@ -2,29 +2,23 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 
-
 const app = express();
 
-app.use(
-    cors({   credentials: true,
-             origin: process.env.FRONTEND_URL
-         }
-    )
-);
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 const sessionOptions = {
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
 };
 if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-    };
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
 }
 app.use(session(sessionOptions));
 app.use(express.json());
 console.log("Hello World!");
 
-app.listen(5000);
+app.listen(4000);
