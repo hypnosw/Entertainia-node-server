927a2a43326a5ddd9c20a1773b68221d5655456c
ccxxyy23 <ccxxyy23@outlook.com>
2023-12-02 20:34:03 -0800

    delete duplicate in app.js

--- app.js
+++ app.js
@@ -5,21 +5,13 @@ import session from "express-session";
 const app = express();
 
 app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+
 const sessionOptions = {
-  secret: "any string",
-  resave: false,
-  saveUninitialized: false,
   secret: "any string",
   resave: false,
   saveUninitialized: false,
 };
 if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-  };
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
