5dc194599aaab54891c642a243bceacbb4eedc76
hypnosw <hypnoswu@gmail.com>
2023-12-02 21:46:20 -0800

    reverted app.js

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 import express from "express";
 import cors from "cors";
 import session from "express-session";
+import mongoose from "mongoose";
+import "dotenv/config.js";
+import Entertainia from "./src/entertainia.js";
 
+// DB_CONNECTION_STRING should be the remote atlas string, LOCAL is local string
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || process.env.LOCAL_DB_STRING;
+mongoose
+    .connect(CONNECTION_STRING)
+    .then(() => {
+      console.log('Connected to the database');
+    })
+    .catch((error) => {
+      console.error('Error connecting to the database:', error);
+    });
 const app = express();
-
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
-
+app.use(
+    cors({   credentials: true,
+           origin: process.env.FRONTEND_URL
+         }
+    )
+);
 const sessionOptions = {
   secret: "any string",
   resave: false,
@@ -22,4 +38,7 @@ app.use(session(sessionOptions));
 app.use(express.json());
 console.log("Hello World!");
 
-app.listen(4000);
+// /hello to test if server is working
+Entertainia(app);
+
+app.listen(process.env.PORT || 5000);
