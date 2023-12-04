32ad0c5cf81408ba34417d116ba554d3a84c539b
ccxxyy23 <ccxxyy23@outlook.com>
2023-12-03 22:55:16 -0800

    edit schema for users

--- userEntity/admins/schema.js
+++ userEntity/admins/schema.js
@@ -1,72 +1,74 @@
 import mongoose from "mongoose";
 
-const adminsSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    // don't need default because it's required on signup
-    // default: "Unknown User",
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+const adminsSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      // don't need default because it's required on signup
+      // default: "Unknown User",
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
 
-  employeeID: {
-    type: String,
-    required: true,
-  },
+    employeeID: {
+      type: String,
+    },
 
-  nickname: {
-    type: String,
-    default: "Unknown User",
-  },
-  profilePicture: {
-    type: Buffer,
-    default: Buffer.from("gf1UcxdHTJ2HQ/EGQrO7mQ==", 'base64'),
-  },
-  personalBio: {
-    type: String,
-    default: "Tell us more about you!",
-  },
-  likedPosts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "posts",
+    nickname: {
+      type: String,
+      default: "Unknown User",
     },
-  ],
-  posts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "posts",
+    profilePicture: {
+      type: Buffer,
+      default: Buffer.from("gf1UcxdHTJ2HQ/EGQrO7mQ==", "base64"),
     },
-  ],
-  following: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "users",
+    personalBio: {
+      type: String,
+      default: "Tell us more about you!",
     },
-  ],
-  followers: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "users",
+    likedPosts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "posts",
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "posts",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    ],
+    followingCount: {
+      type: Number,
+      default: 0,
     },
-  ],
-  followingCount: {
-    type: Number,
-    default: 0,
+    followersCount: {
+      type: Number,
+      default: 0,
+    },
+    role: {
+      type: String,
+      enum: ["ADMIN"],
+      default: "ADMIN",
+      required: true,
+    },
   },
-  followersCount: {
-    type: Number,
-    default: 0,
-  },
-  role: {
-    type: String,
-    enum: ["ADMIN"],
-    default: "ADMIN",
-    required: true,
-  },
-}, {collection:"admins"});
+  { collection: "admins" }
+);
 
 export default adminsSchema;
