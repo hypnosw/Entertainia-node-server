1255e990e804a85c1c1a9f4fb3c4b6a2353c6255
ccxxyy23 <ccxxyy23@outlook.com>
2023-12-07 13:53:44 -0800

    add comment api

--- app.js
+++ app.js
@@ -44,8 +44,6 @@ Entertainia(app);
 UserRoutes(app);
 PostsRoutes(app);
 LikesRoutes(app);
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on port: ${process.env.PORT || 5000}`)});
-
-
-
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server is running on port: ${process.env.PORT || 4000}`);
+});
