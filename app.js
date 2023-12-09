22ed36a3232690c5e1401e59ea230be5fa677183
fengzhihan <fengzhihan98@gmail.com>
2023-12-08 23:09:05 -0800

    latest node

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ Entertainia(app);
 UserRoutes(app);
 PostsRoutes(app);
 // LikesRoutes(app);
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on port: ${process.env.PORT || 5000}`);
+app.listen(process.env.PORT || 5001, () => {
+  console.log(`Server is running on port: ${process.env.PORT || 5001}`);
 });
